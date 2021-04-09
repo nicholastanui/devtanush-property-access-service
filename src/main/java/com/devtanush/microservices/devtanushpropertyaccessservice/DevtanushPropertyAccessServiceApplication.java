d50b444a525d0525703973c2e6497ac132677b52
ntanui <ntanui@safaricom.co.ke>
2021-04-09 13:50:34 +0300

    iitial commit

---
+++ src/main/java/com/devtanush/microservices/devtanushpropertyaccessservice/DevtanushPropertyAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.devtanushpropertyaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtanushPropertyAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtanushPropertyAccessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtanush/microservices/devtanushpropertyaccessservice/DevtanushPropertyAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.devtanushpropertyaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtanushPropertyAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
