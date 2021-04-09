7389f18dd84b93dacbbf53d3971f053d5462ec88
ntanui <ntanui@safaricom.co.ke>
2021-04-09 16:22:19 +0300

    add refresh actuator logic on controller

--- src/main/java/com/devtanush/microservices/devtanushpropertyaccessservice/controller/PropertyFileAccessController.java
+++ src/main/java/com/devtanush/microservices/devtanushpropertyaccessservice/controller/PropertyFileAccessController.java
@@ -3,9 +3,13 @@ package com.devtanush.microservices.devtanushpropertyaccessservice.controller;
 import com.devtanush.microservices.devtanushpropertyaccessservice.beans.PropertyAccessBean;
 import com.devtanush.microservices.devtanushpropertyaccessservice.beans.PropertyAccessValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/access")
@@ -16,6 +20,18 @@ public class PropertyFileAccessController {
 
     @GetMapping("accessPropertyFile")
     public PropertyAccessValue accessPropertyFile() {
+        refreshActuator();
         return new PropertyAccessValue(propertyAccessBean.getName(), propertyAccessBean.getDescription());
+    }
+
+    public void refreshActuator() {
+        RestTemplate restTemplate = new RestTemplate();
+        final String baseUrl = "http://localhost:8100/actuator/refresh";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, "application/json");
+        HttpEntity entity = new HttpEntity(httpHeaders);
+
+        ResponseEntity<String> results = restTemplate.postForEntity(baseUrl, entity, String.class);
     }
 }
